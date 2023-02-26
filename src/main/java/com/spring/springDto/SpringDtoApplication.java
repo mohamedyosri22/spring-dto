7d0cba1af9e173d2f0f1cf344c453bdb7782694a
mhmdyouali <mhmd.you@yahoo.com>
2023-02-26 23:25:57 +0200

    project init

---
+++ src/main/java/com/spring/springDto/SpringDtoApplication.java
@@ -0,0 +1,13 @@
+package com.spring.springDto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDtoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDtoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/springDto/SpringDtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springDto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDtoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
